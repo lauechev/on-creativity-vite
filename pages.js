a0f7863c14dae6f712e5657b52f07f02b4b534b2
lauechev <laura.echeverri3@gmail.com>
2025-01-23 15:16:36 -0500

    🐍 fixed responsivity

--- pages.js
+++ pages.js
@@ -7,30 +7,29 @@ const pages = [
     quote:
       '"The ability to look deeply is the root of creativity. To see past the ordinary and mundane and get to what might otherwise be invisible."',
     author: '- Rick Rubin',
-    color: '#d8a9fc'
+    color: '#d8a9fc',
   },
   {
     quote:
       '"So this, I believe, is the central question upon which all creative living hinges: Do you have the courage to bring forth the treasures that are hidden within you?"',
     author: '- Elizabeth Gilbert',
-    color: '#98b57b'
+    color: '#98b57b',
   },
   {
-    quote:
-      '"Creativity requires faith. Faith requires that we relinquish control."',
+    quote: '"Creativity requires faith. Faith requires that we relinquish control."',
     author: '- Julia Cameron',
-    color: '#f5ad51'
+    color: '#f5ad51',
   },
   {
     quote: `"To live a creative life, we must lose our fear of being wrong"`,
     author: '- Joseph Chilton',
-    color: '#ff82a8'
+    color: '#ff82a8',
   },
   {
     quote: `"Imagination is more important than knowledge"`,
     author: '- Albert Einstein',
-    color: '#6b9fff'
-  }
+    color: '#6b9fff',
+  },
 ];
 
 // pick relevant tags
